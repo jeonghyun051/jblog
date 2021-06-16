dbb1303bbd5d596d95e404751ccc15e29f7f61fb
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-16 16:08:00 +0900

    user security( login, join ), mybatis config

--- jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
@@ -1,24 +1,32 @@
 package com.douzone.jblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.douzone.jblog.service.UserService;
+import com.douzone.jblog.vo.UserVo;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping("/join")
 	public String join() {
 		return "user/join";
 	}
 	
+	@RequestMapping("/joinsuccess")
+	public String join(UserVo vo) {
+		userService.join(vo);
+		return "user/joinsuccess";
+	}
+	
 	@RequestMapping("/login")
 	public String login() {
 		return "user/login";
-	}
-	
-	@RequestMapping("/joinsuccess")
-	public String join(UserVo vo) {
-		return "user/joinsuccess";
 	}
 }
