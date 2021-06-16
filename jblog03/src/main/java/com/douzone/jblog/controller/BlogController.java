620049025cd0cf76b7b80aa82ad253c99d5998f7
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-16 12:39:08 +0900

    jblog03 BlogController multi path 처리

--- jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
@@ -1,9 +1,13 @@
 package com.douzone.jblog.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Optional;
 
-@RequestMapping("/blog")
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/{id:(?!assets).*)}")
 @Controller
 public class BlogController {
 
@@ -11,8 +15,33 @@ public class BlogController {
 	//{id}/{categoryNo}
 	//{id}/{categoryNo}/{postNo}
 	
-	@RequestMapping("")
-	public String index() {
-		return "";
+	@ResponseBody
+	@RequestMapping({"","/{pathNo1}","/{pathNo1}/{pathNo2}"})
+	public String index(
+			@PathVariable("id") String id,
+			@PathVariable("pathNo1") Optional<Long> pathNo1,
+			@PathVariable("pathNo2") Optional<Long> pathNo2) {
+		Long categoryNo = 0L;
+		Long postNo = 0L;
+		
+		if(pathNo2.isPresent()) { // null이 아니다.
+			categoryNo = pathNo1.get();
+			postNo = pathNo2.get();
+		} else if(pathNo1.isPresent()) {
+			categoryNo = pathNo1.get();
+		}
+		
+		System.out.println("id:"+id);
+		System.out.println("category:" + categoryNo);
+		System.out.println("postNo:" + postNo);
+		
+		return "blogController.index";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/admin/basic")
+	public String adminBasic(@PathVariable("id") String id) {
+		System.out.println("id:"+id);
+		return "BlogController.adminBasic";
 	}
 }
